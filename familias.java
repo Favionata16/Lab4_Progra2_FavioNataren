f11d882cde124a8354fbfcd604fbeb8c533ac4fd
Favionata16 <98546416+Favionata16@users.noreply.github.com>
2022-02-11 17:02:52 -0600

    cambios

--- familias.java
+++ familias.java
@@ -1,5 +1,38 @@
 package Lab4P2_FavioNataren_22141210;
 
+import java.util.ArrayList;
+
 public class familias {
+
+    ArrayList<Aldeanos> Aldeanos = new ArrayList();
+    String apellido;
+
+    public familias() {
+    }
+
+    public familias(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public ArrayList<Aldeanos> getAldeanos() {
+        return Aldeanos;
+    }
+
+    public void setAldeanos(Aldeanos aldeanos) {
+        this.Aldeanos.add(aldeanos);
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return ", apellido: " + apellido + " Aldeanos: " + Aldeanos;
+    }
 
 }
